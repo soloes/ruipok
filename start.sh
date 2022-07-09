51e56e6e4581846b10fda879c907e96ed1cf4155
soloes <93706859+soloes@users.noreply.github.com>
2022-07-08 19:11:34 -0600

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/sh
+
+# configs
+AUUID=6e2aa114-523a-4dcb-8713-cbe16998097f
+CADDYIndexPage=https://github.com/Externalizable/bongo.cat/archive/master.zip
+CONFIGCADDY=https://raw.githubusercontent.com/soloes/ruipok/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/soloes/ruipok/master/etc/wry.json
+ParameterSSENCYPT=chacha20-ietf-poly1305
+StoreFiles=https://raw.githubusercontent.com/soloes/ruipok/etc/StoreFiles
+#PORT=4433
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/wry.json
+
+# storefiles
+mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
+wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
+
+for file in $(ls /usr/share/caddy/$AUUID); do
+    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
+done
+
+# start
+tor &
+
+/xray -config /wry.json &
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
